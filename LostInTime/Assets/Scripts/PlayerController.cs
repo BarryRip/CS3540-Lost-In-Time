b0dedaabae1e0aa5bc8d30d8e53f1953fc981630
Eric Yip <yip.e@northeastern.edu>
2024-02-15 13:56:51 -0500

    Updated player movement to have working movement, but still need to fully update camera controls to follow mouse movement

--- LostInTime/Assets/Scripts/PlayerController.cs
+++ LostInTime/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,10 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 10f;
     public float gravity = 9.81f;
     public float airControl = 2f;
-    public Transform playerCamera;
 
     private CharacterController controller;
-    
+    private Vector3 movement;
+
     private bool hasDoubleJumpCharge;
 
     // Start is called before the first frame update
@@ -23,23 +23,101 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckIfLanded();
         Vector3 input = GetNormalizedInput();
-        Vector3 forwardPos = (Camera.main.transform.forward + transform.position);
-        forwardPos = new Vector3(forwardPos.x, transform.position.y, forwardPos.z);
-        transform.LookAt(forwardPos);
-        controller.Move(speed * Time.deltaTime * input);
+        input = CalculateCameraSpaceInput(input);
+        ApplyMovement(input);
+        HandleJump();
+        ApplyGravity();
+        controller.Move(Time.deltaTime * movement);
     }
 
+    private void FixedUpdate()
+    {
+        UpdateStatusWhenGrounded();
+    }
+
+    /// <summary>
+    /// Calculates the world space direction of the given input applied to the
+    /// camera's world space.
+    /// </summary>
+    /// <param name="input">The input direction.</param>
+    /// <returns>The normalized world space input vector based on the camera.</returns>
+    private Vector3 CalculateCameraSpaceInput(Vector3 input)
+    {
+        Vector3 camDirection = Camera.main.transform.TransformDirection(input);
+        camDirection.y = 0;
+        return camDirection.normalized;
+    }
+
+    /// <summary>
+    /// Applies player movement based on the given input.
+    /// </summary>
+    /// <param name="input">The world space input direction to move the player towards.</param>
+    private void ApplyMovement(Vector3 input)
+    {
+        if (controller.isGrounded)
+        {
+            // When grounded, player snaps to face the direction we want, and moves forward.
+            transform.LookAt(transform.position + input);
+            movement = transform.forward * input.magnitude * speed;
+        }
+        else
+        {
+            // When airborne, player has loose air control and slowly faces direction we want.
+            input.y = movement.y;
+            movement = Vector3.Lerp(movement, input, airControl * Time.deltaTime);
+            float rotationAngle = Mathf.LerpAngle(0f, Vector3.Angle(transform.forward, input), Time.deltaTime);
+            transform.LookAt((transform.position + transform.forward));
+        }
+    }
+
+    /// <summary>
+    /// Handles the jump input to apply jumps and double jumps.
+    /// </summary>
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (controller.isGrounded)
+            {
+                movement.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else if (CanDoubleJump() && hasDoubleJumpCharge)
+            {
+                hasDoubleJumpCharge = false;
+                movement.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Apply gravity to the movement vector.
+    /// </summary>
+    private void ApplyGravity()
+    {
+        movement.y -= gravity * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Returns a normalized vector indicating the direction to move the player
+    /// in based on the horizontal and vertical input.
+    /// </summary>
+    /// <returns></returns>
     private Vector3 GetNormalizedInput()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
-        Vector3 input = transform.right * moveHorizontal + transform.forward * moveVertical;
+        Vector3 input = Vector3.right * moveHorizontal + Vector3.forward * moveVertical;
         return input.normalized;
     }
 
-    private void CheckIfLanded()
+    /// <summary>
+    /// Updates the player's status when grounded.
+    /// <para />
+    /// Currently, this will simply give the player a double jump charge when
+    /// grounded.
+    /// </summary>
+    private void UpdateStatusWhenGrounded()
     {
         if (controller.isGrounded)
         {
@@ -47,8 +125,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Check if the player has unlocked the double jump ability.
+    /// <para />
+    /// Currently, this simply returns true. In the future, we should connect
+    /// this to some GameManager script that tracks the powerups collected
+    /// by the player.
+    /// </summary>
+    /// <returns>True if the player has the double jump ability, false otherwise.</returns>
     private bool CanDoubleJump()
     {
-        return false;
+        return true;
     }
 }
