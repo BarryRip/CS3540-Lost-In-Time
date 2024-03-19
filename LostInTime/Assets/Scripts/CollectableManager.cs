d5c949460c048865c8bccfc20ef56f75e1afe376
Ethan Carpenter <ethanCarpenterMail@gmail.com>
2024-03-18 20:54:41 -0400

    uitext update

--- LostInTime/Assets/Scripts/CollectableManager.cs
+++ LostInTime/Assets/Scripts/CollectableManager.cs
@@ -5,23 +5,33 @@ using System;
 
 public class CollectableManager : MonoBehaviour
 {
+    public static string Location1Name = "Field";
+    public static int Location1NumberCollectables = 2;
+    public static string Location2Name = "Town";
+    public static int Location2NumberCollectables = 2;
+    public static string Location3Name = "Castle";
+    public static int Location3NumberCollectables = 2;
+
     public static bool[] collectedIds;
     // total assuming 6 parts per world, subject to change
     public static int CollectablesInWorld = 6;
-    public static int totalCollectables = 6;
+    public static int TotalCollectables = 6;
 
     private static UiTextManager textManager;
 
     // Start is called before the first frame update
     private void Start()
     {
-        textManager.SetCollectableText("Time Machine Parts: " + GetTotalPartsCollected() + "/" + CollectablesInWorld);
+        textManager.SetCollectableText("Time Machine Parts: " + GetTotalPartsCollected() + "/" + CollectablesInWorld + "\n" +
+                                            Location1Name + ": " + GetTotalPartsLocation1() + "/" + Location1NumberCollectables + "\n" +
+                                            Location2Name + ": " + GetTotalPartsLocation2() + "/" + Location2NumberCollectables + "\n" +
+                                            Location3Name + ": " + GetTotalPartsLocation3() + "/" + Location3NumberCollectables);
     }
 
     void Awake()
     {
         textManager = GetComponent<UiTextManager>();
-        collectedIds = new bool[totalCollectables];
+        collectedIds = new bool[TotalCollectables];
     }
 
     public static void GetPart(int id)
@@ -34,9 +44,12 @@ public class CollectableManager : MonoBehaviour
         {
             collectedIds[id] = true;
             textManager.SetNotificationText("Got a time machine part!");
-            textManager.SetCollectableText("Time Machine Parts: " + GetTotalPartsCollected() + "/" + CollectablesInWorld);
+            textManager.SetCollectableText("Time Machine Parts: " + GetTotalPartsCollected() + "/" + CollectablesInWorld + "\n" +
+                                            Location1Name + ": " + GetTotalPartsLocation1() + "/" + Location1NumberCollectables + "\n" +
+                                            Location2Name + ": " + GetTotalPartsLocation2() + "/" + Location2NumberCollectables + "\n" +
+                                            Location3Name + ": " + GetTotalPartsLocation3() + "/" + Location3NumberCollectables);
 
-            if (GetTotalPartsCollected() == totalCollectables)
+            if (GetTotalPartsCollected() == TotalCollectables)
             {
                 textManager.SetNotificationText("You Win!");
             }
@@ -46,5 +59,20 @@ public class CollectableManager : MonoBehaviour
     private static int GetTotalPartsCollected()
     {
         return Array.FindAll(collectedIds, (bool x) => { return x; }).Length;
+    }
+
+    private static int GetTotalPartsLocation1()
+    {
+        return Array.FindAll(new ArraySegment<bool>(collectedIds, 0, Location1NumberCollectables).ToArray(), (bool x) => { return x; }).Length;
+    }
+
+    private static int GetTotalPartsLocation2()
+    {
+        return Array.FindAll(new ArraySegment<bool>(collectedIds, Location1NumberCollectables, Location2NumberCollectables).ToArray(), (bool x) => { return x; }).Length;
+    }
+
+    private static int GetTotalPartsLocation3()
+    {
+        return Array.FindAll(new ArraySegment<bool>(collectedIds, Location1NumberCollectables + Location2NumberCollectables, Location3NumberCollectables).ToArray(), (bool x) => { return x; }).Length;
     }
 }
