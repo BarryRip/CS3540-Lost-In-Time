28403f7e873886c38f98e526b1e0deb1390fb710
redSoar <132908031+redSoar@users.noreply.github.com>
2024-04-04 05:21:12 -0400

    Void Out (kinda)
    added clouds back as well as void out mechanic (does not work yet)

---
+++ LostInTime/Assets/Scripts/VoidOut.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidOut : MonoBehaviour
+{
+    Vector3 spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = spawnPoint;
+        }
+    }
+}
