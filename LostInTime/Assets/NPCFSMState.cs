c40fa7370bbfd74f3d3d178dba9fc6839aa1daa1
Diego Garcia <garcia.dieg@northeastern.edu>
2024-04-09 03:10:45 -0400

    Got the FSM working again
    NPC can now walk using the Nav Mesh

--- LostInTime/Assets/NPCFSMState.cs
+++ LostInTime/Assets/NPCFSMState.cs
@@ -7,26 +7,17 @@ using Random = UnityEngine.Random;
 
 public class NPCFSMState : MonoBehaviour
 {
-
     public GameObject[] wanderPoints;
     public float NPCSpeed = 5;
     public float chaseDistance = 10;
 
-    Animator anim;
+    private Animator anim;
+    private int currentDestinationIndex = 0;
+
+    private GameObject player;
+    private Transform enemyEyes;
+    private NavMeshAgent agent;
     Vector3 nextDestination;
-    int currentDesinationindex = 0;
-
-
-    public GameObject player;
-    public float fieldOFView = 45f;
-    float elapseTime = 0;
-
-    Transform deadTransform;
-
-    NavMeshAgent agent;
-    private NpcInteraction interact;
-    public Transform enemyEyes;
-
     public enum FSMStates
     {
         Patrol,
@@ -35,21 +26,18 @@ public class NPCFSMState : MonoBehaviour
 
     public FSMStates currentState;
 
-    // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        wanderPoints = GameObject.FindGameObjectsWithTag("Wanderpoint");
+        enemyEyes = transform; // Assuming NPC's eyes are its own transform
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 
         Initialize();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         switch (currentState)
         {
             case FSMStates.Patrol:
@@ -59,7 +47,6 @@ public class NPCFSMState : MonoBehaviour
                 UpdateTalkState();
                 break;
         }
-        elapseTime += Time.deltaTime;
     }
 
     void Initialize()
@@ -70,32 +57,25 @@ public class NPCFSMState : MonoBehaviour
 
     void UpdatePatrolState()
     {
-        print("Patroling!");
         anim.SetInteger("animState", 2);
-
-        agent.stoppingDistance = 0;
-
         agent.speed = 2f;
 
-        if (Vector3.Distance(transform.position, nextDestination) < 1)
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
             FindNextPoint();
-
-        } else if (Input.GetKeyDown(KeyCode.E) || IsPlayerInClearFOV())
+        }
+        else if (Input.GetKeyDown(KeyCode.E) || IsPlayerInClearFOV())
         {
             anim.SetInteger("animState", 0);
             FaceTarget(player.transform.position);
             currentState = FSMStates.Talk;
             agent.speed = 0;
         }
-
-        FaceTarget(nextDestination);
-
-        agent.SetDestination(nextDestination);
-
+        else
+        {
+            FaceTarget(nextDestination);
+        }
     }
-
-    
 
     void UpdateTalkState()
     {
@@ -110,10 +90,8 @@ public class NPCFSMState : MonoBehaviour
 
     void FindNextPoint()
     {
-        nextDestination = wanderPoints[currentDesinationindex].transform.position;
-
-        currentDesinationindex = (currentDesinationindex + 1) % wanderPoints.Length;
-
+        currentDestinationIndex = (currentDestinationIndex + 1) % wanderPoints.Length;
+        nextDestination = wanderPoints[currentDestinationIndex].transform.position;
         agent.SetDestination(nextDestination);
     }
 
@@ -125,33 +103,17 @@ public class NPCFSMState : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
     }
 
-
-
-    private void OnDrawGizmos()
-    {
-
-        Vector3 frontRayPoint = enemyEyes.position + (enemyEyes.forward * chaseDistance);
-        Vector3 leftRayPoint = Quaternion.Euler(0, fieldOFView * 0.5f, 0) * frontRayPoint;
-        Vector3 rightRayPoint = Quaternion.Euler(0, -fieldOFView * 0.5f, 0) * frontRayPoint;
-
-        Debug.DrawLine(enemyEyes.position, frontRayPoint, Color.cyan);
-        Debug.DrawLine(enemyEyes.position, leftRayPoint, Color.yellow);
-        Debug.DrawLine(enemyEyes.position, rightRayPoint, Color.magenta);
-    }
-
     bool IsPlayerInClearFOV()
     {
         RaycastHit hit;
         Vector3 directionToPlayer = player.transform.position - enemyEyes.position;
 
-        if (Vector3.Angle(directionToPlayer, enemyEyes.forward) <= fieldOFView)
+        if (Vector3.Angle(directionToPlayer, enemyEyes.forward) <= 45f)
         {
             if (Physics.Raycast(enemyEyes.position, directionToPlayer, out hit, chaseDistance))
             {
                 if (hit.collider.CompareTag("Player"))
                 {
-                    print("Player in sight!");
-                    agent.speed = 0f;
                     return true;
                 }
                 return false;
